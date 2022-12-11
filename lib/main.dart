d7e36b6dc8a4634865eb990d331f8bcebe2d0685
walther <waltherleonardo@gmail.com>
2022-12-11 15:51:43 -0500

    genesis commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter starting point',
+      darkTheme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('home page'),
+      ),
+    );
+  }
+}
