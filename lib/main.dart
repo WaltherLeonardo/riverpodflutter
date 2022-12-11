5576f42ac0583543e3848e712b4df018133f3ea9
walther <waltherleonardo@gmail.com>
2022-12-11 18:58:28 -0500

    consumer stateful notifier

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,15 @@ class App extends StatelessWidget {
       darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
-      home: const HomePage(),
+      home: const HomePage2(),
     );
   }
 }
 
+/*
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +40,35 @@ class HomePage extends StatelessWidget {
             return Text(p);
           },
         ),
+      ),
+    );
+  }
+}
+*/
+
+class HomePage2 extends ConsumerStatefulWidget {
+  const HomePage2({Key? key}) : super(key: key);
+
+  @override
+  ConsumerState<HomePage2> createState() => _HomePage2State();
+}
+
+class _HomePage2State extends ConsumerState<HomePage2> {
+  @override
+  void initState() {
+    super.initState();
+    final p = ref.read(
+        provider01); //<-we use re.read because we are outside the build method
+    print('this is the state on cath on');
+    print('initState lifecicle: $p');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final p2 = ref.watch(provider01);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(p2),
       ),
     );
   }
